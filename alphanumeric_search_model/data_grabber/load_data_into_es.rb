8379438a35a6b02f9920f9c002abe72485e45e0b
Kit Summers <kit.summers@gsa.gov>
2023-05-23 12:39:01 -0600

    Initial commit

---
+++ alphanumeric_search_model/data_grabber/load_data_into_es.rb
@@ -0,0 +1,18 @@
+# See Confluence for full details.
+# https://cm.usa.gov/confluence/display/SRCH/Alpha+Numeric+String+Search+Redesign+and+Improvements
+
+
+# Commands to load snaphots buckets
+# Load i14y indices
+# Load Logstash indices
+
+# Commands to load indices
+# Did this from the Kibana interface TODO: Lookup how to do this!!!!
+
+# Reindex i14y adding keyword field to relevent text fields
+# Did do a reindex to add a whitespace analyzer on relevent text fields, I don't know if this was necessary?
+# This takes 12 to 24 hours depending on cluster size
+
+# Remove aliases from i14y indices
+
+# Add aliases to the new index
