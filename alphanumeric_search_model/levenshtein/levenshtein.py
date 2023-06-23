caceb7ab65654df4390311eecb9082d11a57aba8
Kit Summers <kit.summers@gsa.gov>
2023-06-22 21:40:25 -0600

    Compute levenshtein distance between two lists

--- alphanumeric_search_model/levenshtein/levenshtein.py
+++ alphanumeric_search_model/levenshtein/levenshtein.py
@@ -13,6 +13,7 @@
 # Do Levenshtein Compare between lists (currently an n^n operation, would there be a faster way?)
 
 import requests
+from Levenshtein import ratio
 import json
 
 query1 = {
@@ -32,7 +33,7 @@ query2 = {
         "regex_patterns" : {
             "terms": {
                 "field": "regex_patterns.keyword",
-                "size": 50
+                "size": 500
             }
         }
     },
@@ -49,6 +50,8 @@ query2 = {
 i14y_list = []
 logstash_list = []
 
+distance_touple = []
+
 es_url = "http://localhost:9200/"
 
 def query_elasticsearch(search_endpoint, body = ""):
@@ -60,7 +63,9 @@ def query_elasticsearch(search_endpoint, body = ""):
     return r
 
 def get_levenshtein_distance(word_one, word_two):
-    return 0
+    lratio = ratio(word_one, word_two, score_cutoff=0.7)
+    if lratio > 0.0:
+        distance_touple.append([word_one, word_two, lratio])
 
 def generate_list_from_i14y():
     search_endpoint = "production-i14y-documents-searchgov-v6-reindex_keyword_regex/_search"
@@ -90,10 +95,14 @@ def generate_list_from_logstash_requests():
 
 
 #query_elasticsearch("production-i14y-documents-searchgov-v6-reindex_keyword_regex/_search", json.dumps(query1))
-# generate_list_from_i14y()
+generate_list_from_i14y()
 # some_list = i14y_list.sort()
-# print(some_list)
-# print(len(set(i14y_list)))
+print(i14y_list)
+print(len(set(i14y_list)))
 generate_list_from_logstash_requests()
 print(logstash_list)
 print(len(set(logstash_list)))
+for i14y in set(i14y_list):
+    for logstash_word in set(logstash_list):
+        get_levenshtein_distance(i14y, logstash_word)
+print(*distance_touple, sep='\n')
