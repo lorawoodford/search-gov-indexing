29b38c0db59be49da1518ee91d0d6fda4a527f56
Kit Summers <kit.summers@gsa.gov>
2023-08-29 08:02:55 -0600

    Initial tests

---
+++ alphanumeric_search_model/spacy_model_training/test/test.py
@@ -0,0 +1,26 @@
+
+def test_query_elasticsearch():
+    # Query elasticsearch
+    # Assert that Elasticsearch responds with a 200
+
+def test_generate_list_from_i14y():
+    # Probably best way to handle this is to generate a list from the top five domains and ensure they are the same
+
+def test_get_test_document_from_elasticsearch():
+    # Use a value to search for a document
+    # Assert that a document is returned by ES 
+
+def test_generate_rules():
+    # This creates an entity ruler and save to disk...
+
+def test_create_training_data():
+    # Create a list of values and a data type to go with
+    # Assert that all values have the appropriate data type
+
+def test_test_model():
+    # Not sure how to test this, will need to look at the return values
+
+def test_remove_english_words_from_list():
+    # Create a list of english and non english words
+    # Assert that the english words have been removed
+
