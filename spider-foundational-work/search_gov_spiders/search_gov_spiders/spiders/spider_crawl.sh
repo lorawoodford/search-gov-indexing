7d1a6c33d8d6bfd49035ca2119ac41ac175da0b1
Chance Crawford <ccrawford@fearless.tech>
2023-12-06 13:12:11 -0500

    Add bash script for running python scripts via screen

---
+++ spider-foundational-work/search_gov_spiders/search_gov_spiders/spiders/spider_crawl.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change to the directory containing the Python script
+cd ~/search-gov-indexing/spider-foundational-work/search-gov-spiders/search-gov-spiders/spiders || exit
+
+# Run the Python script
+python "$1.py"
