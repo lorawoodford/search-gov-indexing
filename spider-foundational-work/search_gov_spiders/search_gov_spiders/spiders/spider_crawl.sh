6868a7459cbb8083e55882de38d2eaddbf932cf5
Chance Crawford <ccrawford@fearless.tech>
2024-01-05 15:06:38 -0500

    add abstracted spider to take any domain/start url, add requirements.txt

--- spider-foundational-work/search_gov_spiders/search_gov_spiders/spiders/spider_crawl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Change to the directory containing the Python script
-cd ~/search-gov-indexing/spider-foundational-work/search-gov-spiders/search-gov-spiders/spiders || exit
-
-# Run the Python script
-python "$1.py"
